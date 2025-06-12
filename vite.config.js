96cc58dd1b5c4a0b1a5cfacda61dc5acc0690a8b
Ammad Ahmed <ttorfidoo001@gmail.com>
2025-06-12 10:58:25 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/home/',
+  base: '/home/', // your repo name
   plugins: [react()],
+  build: {
+    outDir: 'docs' // GitHub Pages will serve from this folder
+  },
   optimizeDeps: {
     esbuildOptions: {
       loader: {
